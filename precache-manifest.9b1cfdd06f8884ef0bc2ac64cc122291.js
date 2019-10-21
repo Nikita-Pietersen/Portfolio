2a492ddc091575aa9b19767c810de9ed41424bfb
Nikita <nikitap544@gmail.com>
2019-10-21 16:47:17 +0200

    Updates

---
+++ precache-manifest.9b1cfdd06f8884ef0bc2ac64cc122291.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "92214d49ff30745c77cf3f26b8d921b2",
+    "url": "/Portfolio/CV/index.html"
+  },
+  {
+    "revision": "3b3c78b1afe45c2d4bf6",
+    "url": "/Portfolio/CV/static/css/main.6fcdd212.chunk.css"
+  },
+  {
+    "revision": "372ee8fccecacd479f6c",
+    "url": "/Portfolio/CV/static/js/2.cb48e588.chunk.js"
+  },
+  {
+    "revision": "3b3c78b1afe45c2d4bf6",
+    "url": "/Portfolio/CV/static/js/main.56417a47.chunk.js"
+  },
+  {
+    "revision": "c2a6ee34490886ea59f7",
+    "url": "/Portfolio/CV/static/js/runtime-main.44b8164f.js"
+  },
+  {
+    "revision": "9ec5f6f9ecc93f520b96c48488f8e0e2",
+    "url": "/Portfolio/CV/static/media/Game.9ec5f6f9.jpg"
+  },
+  {
+    "revision": "1e1ad4ae03e61acfe48df78544f06b61",
+    "url": "/Portfolio/CV/static/media/Shop.1e1ad4ae.jpg"
+  },
+  {
+    "revision": "3e4f5acede1e6cb4b8a9a62a0c81339e",
+    "url": "/Portfolio/CV/static/media/Weather App.3e4f5ace.jpg"
+  },
+  {
+    "revision": "770c6a5a6a811a623caabce4f23967d9",
+    "url": "/Portfolio/CV/static/media/back.770c6a5a.jpg"
+  },
+  {
+    "revision": "f48fa7c900f64bbf6b7d0f4de1bd6cf5",
+    "url": "/Portfolio/CV/static/media/profileImage.f48fa7c9.jpg"
+  }
+]);
