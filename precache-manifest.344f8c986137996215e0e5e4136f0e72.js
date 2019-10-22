b014397b51aae8a73a5963c26021c64cea27b229
Nikita Pietersen <nikitap544@gmail.com>
2019-10-22 09:10:57 +0200

    Updates

---
+++ precache-manifest.344f8c986137996215e0e5e4136f0e72.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "13a9e74ea13f7314154e2d139cc6bb33",
+    "url": "/Portfolio/index.html"
+  },
+  {
+    "revision": "62e862afe9ce837186bd",
+    "url": "/Portfolio/static/css/main.6e7906f1.chunk.css"
+  },
+  {
+    "revision": "372ee8fccecacd479f6c",
+    "url": "/Portfolio/static/js/2.cb48e588.chunk.js"
+  },
+  {
+    "revision": "62e862afe9ce837186bd",
+    "url": "/Portfolio/static/js/main.57212de7.chunk.js"
+  },
+  {
+    "revision": "63fe68486ecb758e5a56",
+    "url": "/Portfolio/static/js/runtime-main.b0957d76.js"
+  },
+  {
+    "revision": "9ec5f6f9ecc93f520b96c48488f8e0e2",
+    "url": "/Portfolio/static/media/Game.9ec5f6f9.jpg"
+  },
+  {
+    "revision": "1e1ad4ae03e61acfe48df78544f06b61",
+    "url": "/Portfolio/static/media/Shop.1e1ad4ae.jpg"
+  },
+  {
+    "revision": "3e4f5acede1e6cb4b8a9a62a0c81339e",
+    "url": "/Portfolio/static/media/Weather App.3e4f5ace.jpg"
+  },
+  {
+    "revision": "770c6a5a6a811a623caabce4f23967d9",
+    "url": "/Portfolio/static/media/back.770c6a5a.jpg"
+  },
+  {
+    "revision": "f48fa7c900f64bbf6b7d0f4de1bd6cf5",
+    "url": "/Portfolio/static/media/profileImage.f48fa7c9.jpg"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/Portfolio/CV/precache-manifest.9b1cfdd06f8884ef0bc2ac64cc122291.js"
+  "/Portfolio/precache-manifest.344f8c986137996215e0e5e4136f0e72.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/Portfolio/CV/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/Portfolio/index.html"), {
   
   blacklist: [/^\/_/,/\/[^\/?]+\.[^\/]+$/],
 });
